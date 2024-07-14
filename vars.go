4e143b916a14bb7778448a0d28298f39c7675bc7
Tinkerer <tinkerer@zappem.net>
2024-07-14 14:21:02 -0700

    Add support for Snapshot handling.
    Signed-off-by: Tinkerer <tinkerer@zappem.net>

--- vars.go
+++ vars.go
@@ -28,6 +28,7 @@ func New() *Metrics {
 var (
 	ErrInvalid   = errors.New("undefined metrics")
 	ErrNotNumber = errors.New("not a number")
+	ErrNotFound  = errors.New("not found")
 )
 
 // Set sets the value of a specific metric.
@@ -51,10 +52,9 @@ func (m *Metrics) Get(k string) interface{} {
 	return m.Detail[k]
 }
 
-// getNumberLocked returns a numerical value for a specific metric, or
-// an error.
-func (m *Metrics) getNumberLocked(k string) (float64, error) {
-	v := m.Detail[k]
+// AsNumber returns a numerical value for an interface{} value, or an
+// error.
+func AsNumber(v interface{}) (float64, error) {
 	switch v.(type) {
 	case int:
 		return float64(v.(int)), nil
@@ -82,45 +82,27 @@ func (m *Metrics) GetNumber(k string) (float64, error) {
 		return 0, ErrNotNumber
 	}
 	m.mu.Lock()
-	defer m.mu.Unlock()
-	return m.getNumberLocked(k)
+	v := m.Detail[k]
+	m.mu.Unlock()
+	return AsNumber(v)
 }
 
-// Add ads one to a metric or, in the case the metric was not
+// Add adds a number to a metric or, in the case the metric was not
 // previously numerical, it replaces the metric with the provided
-// number.
+// number, n.
 func (m *Metrics) Add(k string, n float64) {
 	if m == nil {
 		return
 	}
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	x, err := m.getNumberLocked(k)
-	if err != nil {
+	x, ok := m.Detail[k]
+	v, err := AsNumber(x)
+	if !ok || err != nil {
 		m.Detail[k] = n
 	} else {
-		m.Detail[k] = n + x
+		m.Detail[k] = n + v
 	}
-}
-
-// Snapshot holds a timestamped snapshot of metrics.
-type Snapshot struct {
-	When   time.Time
-	Values *Metrics
-}
-
-// Snap snapshots all of the current metric values.
-func (m *Metrics) Snap() *Snapshot {
-	s := &Snapshot{
-		Values: New(),
-	}
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	for k, v := range m.Detail {
-		s.Values.Detail[k] = v
-	}
-	s.When = time.Now()
-	return s
 }
 
 // DumpMDTable returns a byte array of markdown text that represents a
@@ -141,4 +123,162 @@ func (m *Metrics) DumpMDTable() []byte {
 	}
 
 	return []byte(strings.Join(append([]string{fmt.Sprintf("key | value at %s\n----|------", s.When.Format(time.UnixDate))}, ks...), "\n") + "\n")
+}
+
+// Snapshot holds a timestamped snapshot of metrics.
+type Snapshot struct {
+	When   time.Time
+	Values *Metrics
+}
+
+// Snap snapshots all of the current metric values.
+func (m *Metrics) Snap() *Snapshot {
+	s := &Snapshot{
+		Values: New(),
+	}
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	s.When = time.Now()
+	for k, v := range m.Detail {
+		s.Values.Detail[k] = v
+	}
+	return s
+}
+
+// Trim removes redundant entries from an array of Snapshots.  The
+// returned value includes the most recently valid timestamp for all
+// entries. That is, the most recent snapshot of the trimmed slice is
+// a full snapshot. The slice is edited in place and the length of the
+// slice may also reduce.
+func Trim(snaps []*Snapshot) (results []*Snapshot) {
+	latest := make(map[string]string)
+	for i := 0; i < len(snaps)-1; i++ {
+		m := snaps[i].Values
+		var ks []string
+		m.mu.Lock()
+		for k, v := range m.Detail {
+			s := fmt.Sprint(v)
+			if latest[k] == s {
+				ks = append(ks, k)
+			} else {
+				latest[k] = s
+			}
+		}
+		for _, k := range ks {
+			delete(m.Detail, k)
+		}
+		m.mu.Unlock()
+		if len(m.Detail) == 0 {
+			snaps = append(snaps[:i], snaps[i+1:]...)
+		}
+	}
+	results = snaps
+	return
+}
+
+// Infer returns the most current value for a specified key at the
+// requested time, indicating the time when the returned value was
+// recorded.
+func Infer(snaps []*Snapshot, t time.Time, k string) (index int, v interface{}, err error) {
+	if len(snaps) == 0 || snaps[0].When.After(t) {
+		err = ErrNotFound
+		return
+	}
+	before := sort.Search(len(snaps), func(a int) bool {
+		return snaps[a].When.After(t)
+	})
+	var ok bool
+	for i := before - 1; before > 0; before-- {
+		if v, ok = snaps[i].Values.Detail[k]; ok {
+			index = i
+			return
+		}
+	}
+	err = ErrNotFound
+	return
+}
+
+// ExtractNumbers returns an array of number values. The first column
+// holds the number of timeunits since the epoch associated with the
+// measured value.
+func ExtractNumbers(snaps []*Snapshot, timeunits time.Duration, from, to time.Time, vars []string) ([][]float64, error) {
+	starts := make(map[string]float64)
+	var minI int
+	for j, k := range vars {
+		i, v, err := Infer(snaps, from, k)
+		if err != nil {
+			return nil, fmt.Errorf("error for %q at %v: %v", k, from, err)
+		}
+		n, err := AsNumber(v)
+		if err != nil {
+			return nil, fmt.Errorf("error for %q at %v: %v", k, from, err)
+		}
+		if j == 0 || i > minI {
+			minI = i
+		}
+		starts[k] = n
+	}
+	lastTS := float64(0)
+	ts := float64(from.UnixNano() / int64(timeunits))
+	var lines [][]float64
+	done := false
+	for i := minI + 1; i <= len(snaps); i++ {
+		vs := []float64{ts}
+		for _, k := range vars {
+			vs = append(vs, starts[k])
+		}
+		if ts == lastTS {
+			lines[len(lines)-1] = vs
+		} else {
+			lines = append(lines, vs)
+		}
+		if done || i == len(snaps) {
+			break
+		}
+		lastTS = ts
+		s := snaps[i]
+		if s.When.Before(to) {
+			ts = float64(s.When.UnixNano() / int64(timeunits))
+		} else if tts := float64(to.UnixNano() / int64(timeunits)); ts == tts {
+			break
+		} else {
+			ts = tts
+			done = true
+			continue
+		}
+		for k, x := range s.Values.Detail {
+			v, err := AsNumber(x)
+			if err != nil {
+				return nil, fmt.Errorf("snapshot[%d][%q] = %v: %v", i, k, x, err)
+			}
+			starts[k] = v
+		}
+	}
+	return lines, nil
+}
+
+// Sample holds an (Timestamp,X) value.
+type Sample struct {
+	When  time.Time
+	Value float64
+}
+
+// Rate is a convenience function for determining the rate of change
+// of some variable, at v[1]. It tries to compute the gradient of the
+// two points around the center point, but fails over to a best guess
+// based on two or fewer samples.
+func Rate(v ...Sample) float64 {
+	if len(v) <= 1 {
+		return 0
+	}
+	t0 := float64(v[0].When.UnixNano())
+	var t1, v1 float64
+	if len(v) == 2 {
+		t1 = float64(v[1].When.UnixNano())
+		v1 = v[1].Value
+	} else {
+		t1 = float64(v[2].When.UnixNano())
+		v1 = v[2].Value
+	}
+	return (v1 - v[0].Value) / (t1 - t0) * float64(time.Second/time.Nanosecond)
 }
--- vars_test.go
+++ vars_test.go
@@ -2,6 +2,7 @@ package vars
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"testing"
 	"time"
@@ -95,6 +96,83 @@ func TestDumpMDTable(t *testing.T) {
 	for i, x := range lines[1:4] {
 		if s := string(x); s != expect[i] {
 			t.Errorf("got=%q want=%q", s, expect[i])
+		}
+	}
+}
+
+func TestSnaps(t *testing.T) {
+	vs := New()
+	for i := 10; i < 16; i++ {
+		vs.Set(fmt.Sprintf("%X", i), i)
+	}
+	snaps := []*Snapshot{vs.Snap()}
+	for i := 0; i < 48; i++ {
+		time.Sleep(1 * time.Millisecond)
+		vs.Add(fmt.Sprintf("%X", 10+(i%6)), float64(i))
+		snaps = append(snaps, vs.Snap())
+		snaps = Trim(snaps)
+		delta := 0
+		if i >= 1 {
+			delta = -1
+		}
+		if i > 1 && len(snaps[i-1].Values.Detail) != 1 {
+			t.Errorf("for [%d] expecting 1 entry, got %v", i, snaps[i-1].Values.Detail)
+		}
+		if got, want := len(snaps), 2+delta+i; got != want {
+			t.Fatalf("wrong number of snapshots: got=%d, want=%d", got, want)
+		}
+	}
+
+	from := snaps[0].When
+	to := snaps[len(snaps)-1].When
+	ks := []string{"A", "C", "F"}
+	nums, err := ExtractNumbers(snaps, time.Millisecond, from, to, ks)
+	if err != nil {
+		t.Errorf("ExtractNumbers failed: %v", err)
+	}
+	if got, want := len(nums), len(snaps); got != want {
+		t.Errorf("bad number of numbers: got=%d, want=%d", got, want)
+	}
+
+	lastTS := 0.0
+	for i := 0; i < len(nums); i++ {
+		a := nums[i]
+		if a[0] <= lastTS {
+			t.Errorf("[%d] non increasing timestamp: got=%f, want>%f", i, a[0], lastTS)
+		}
+		lastTS = a[0]
+		if i == 0 {
+			continue
+		}
+		changed := 0
+		for j := 1; j < len(a); j++ {
+			if was, is := nums[i-1][j], a[j]; was < is {
+				changed++
+			}
+		}
+		if changed > 1 {
+			t.Errorf("[%d] too many changed, %d, want <= 1", i, changed)
+		}
+	}
+}
+
+func TestRate(t *testing.T) {
+	samples := []struct {
+		dt time.Duration
+		v  float64
+		r  float64
+	}{
+		{dt: time.Second, v: 1, r: 0},
+		{dt: 2 * time.Second, v: 2, r: 1},
+		{dt: 5 * time.Second, v: 4, r: 0.75},
+	}
+	now := time.Now()
+	var pts []Sample
+	for i, s := range samples {
+		x := Sample{When: now.Add(s.dt), Value: s.v}
+		pts = append(pts, x)
+		if got, want := Rate(pts...), s.r; got != want {
+			t.Errorf("[%d] mismatch: got=%f, want=%f", i, got, want)
 		}
 	}
 }
